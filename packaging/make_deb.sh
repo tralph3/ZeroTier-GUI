4bfb63165891c0c994c55df9178ba9645916d13e
Tomás Ralph <tralph3@T430.localdomain>
2021-04-29 20:03:08 -0300

    Added script for .deb creation

---
+++ packaging/make_deb.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# This script will create a deb package for ZeroTier-GUI
+
+cd ..
+packageVersion=$(printf "1.2.1.r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+cd -
+
+# Create file structure
+
+mkdir -v DEBIAN
+mkdir -pv usr/share/{licenses/ZeroTier-GUI,pixmaps,applications,doc/ZeroTier-GUI}
+mkdir -pv usr/bin
+
+
+# Copy files to corresponding directories
+
+cp -vf ../LICENSE usr/share/licenses/ZeroTier-GUI
+cp -vf ../img/zerotier-gui.png usr/share/pixmaps
+cp -vf ../zerotier-gui.desktop usr/share/applications
+cp -vf ../README.md usr/share/doc/ZeroTier-GUI
+cp -vf ../src/zerotier-gui usr/bin
+
+
+# Create control file
+
+echo "Package: zerotier-gui
+Version: ${packageVersion}
+Architecture: all
+Maintainer: tralph3
+Depends: python3-tk,policykit-1,python3
+Priority: optional
+Homepage: https://github.com/tralph3/ZeroTier-GUI
+Description: A Linux front-end for ZeroTier" > DEBIAN/control
+
+# Build the package
+
+dpkg-deb --build ../packaging
